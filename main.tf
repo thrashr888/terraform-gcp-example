9f25661275fc7edb63b8e2993db15269379a337b
Paul Thrasher <thrashr888@gmail.com>
2024-01-18 17:55:57 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,15 @@ variable "project" {
   default = "tfce-test"
 }
 
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.12.0"
+    }
+  }
+}
+
 provider "google" {
   project     = var.project
   region      = var.zone
